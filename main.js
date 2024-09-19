6a8ecda7e4867d3585260bf6b9cf64a02bdd6fe9
Keuznub <breixopastoriza@gmail.com>
2024-09-19 09:24:09 +0200

    Posterior version

--- main.js
+++ main.js
@@ -1,40 +1,38 @@
-import getNoteFromKey from './noteUtils'
+import getNoteFromKey, {synth} from './noteUtils'
 import './style.css'
 import * as Tone from "tone"
 
-const synth = new Tone.Sampler({
-	urls: {
-		C4: "sound/C4.mp3",
-		D4: "sound/D4.mp3",
-		F4: "sound/F4.mp3",
-		A4: "sound/A4.mp3",
-	},
-	release: 1,
-	baseUrl: "https://tonejs.github.io/audio/salamander/",
-}).toDestination();
+const synth = new Tone.Synth().toDestination();
 
 let keys = document.querySelectorAll('.key')
 for(let key of keys){
   let dataNote = key.getAttribute('data-note')
   key.addEventListener('mousedown', () => playNote(dataNote))
-  key.addEventListener('mouseup', () => stopNote)
-  key.addEventListener('mouseleave', () => stopNote)
+  key.addEventListener('mouseup', stopNote)
+  key.addEventListener('mouseleave', stopNote)
 }
 
-document.addEventListener("keypress", ctrlTeclado)
-document.addEventListener("keyrelease", stopNote)
+document.addEventListener("keydown", ctrlTeclado)
+document.addEventListener("keyup", stopNote)
 
 function ctrlTeclado(event){
   let note = getNoteFromKey(event.key)
+  let key = document.querySelector('[data-note="'+note+'"]')
+  key && key.classList.add('pressed')
   playNote(note)
 
 }
 
 
 function playNote(note){
-  synth.triggerAttackRelease(note);
+  console.log(note)
+  synth.triggerAttack(note);
 }
 
-function stopNote(){
+function stopNote(event){
+  console.log("Release")
+  let note = getNoteFromKey(event.key)
+  let key = document.querySelector('[data-note="'+note+'"]')
+  key.classList.remove('pressed')
   synth.triggerRelease();
 }
--- noteUtils.js
+++ noteUtils.js
@@ -16,6 +16,7 @@ const map = {
 }
 
 
+
 export default function getNoteFromKey(key){
     if(!key) return 
     return map[key.toLowerCase()]
